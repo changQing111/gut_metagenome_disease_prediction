412f64ddaee7e54a7f6ba6e992b29fb80a1d26c9
Qing Chang <46983747+changQing111@users.noreply.github.com>
2023-02-08 19:58:33 +0800

    Update main.R

--- plot_metrics/main.R
+++ plot_metrics/main.R
@@ -1,6 +1,7 @@
 suppressPackageStartupMessages(library("tidyverse"))
 suppressPackageStartupMessages(library("optparse"))
 suppressPackageStartupMessages(library("Rmisc")) 
+#options (warn = -1) # ignore warnings 
 
 # load function
 source("func.R")
@@ -12,19 +13,17 @@ target_dir1 = "metaphlan_train_res" # args1
 target_dir2 = "kssd_train_res"  # args2
 tools1 = "metaphlan3"
 tools2 = "kssd"
-plot_type = "boxplot"
+
 # parse args
 parser <- OptionParser(description = "kssd and metaphlan3 comparison")
 parser <- add_option(parser, c("--input_dir1"), default=target_dir1, help="Input first dir")
 parser <- add_option(parser, c("--input_dir2"), default=target_dir2, help="Input second dir")
-parser <- add_option(parser, c("--tools1"), default=tools1, help="Input first tool")
-parser <- add_option(parser, c("--tools2"), default=tools2, help="Input second tool")
+parser <- add_option(parser, c("--tools1"), default=tools1, help="Input first tool, default: metaphlan3")
+parser <- add_option(parser, c("--tools2"), default=tools2, help="Input second tool, default: kssd")
 parser <- add_option(parser, c("-m", "--metric"), type = "character", 
-                     help="all(accuracy, precision, recall, F1, AUC) or ROC")
+                     help="all ( accuracy, precision, recall, F1, AUC ) or ROC")
 parser <- add_option(parser, c("-s", "--set"), default = data_set, type = "character", help = "data set, 
                             If multiple entries are entered, separate them with commas")
-parser <- add_option(parser, c("-p", "--plot"), default=plot_type, type="character", 
-                            help="choose a plot type (boxplot or barplot)")
 parser <- add_option(parser, c("-o", "--out"), type = "character", help="out file name")
 
 parse <- parse_args(parser)
@@ -41,14 +40,13 @@ t1 <- parse$tools1
 t2 <- parse$tools2
 m <- parse$metric
 s <- str_split(parse$set, ",") %>% unlist()
-plt <- parse$plot
 o <- parse$out
 
 if(!dir.exists(o)) {
   dir.create(o)
 }
 
-# metrics boxplot or barplot
+# metrics boxplot
 if(m != "ROC") {
   all_metric_li <- lapply(s, function(x) { 
                               get_metric_df(disease = x, 
@@ -57,22 +55,18 @@ if(m != "ROC") {
                                             tools_1 = t1, 
                                             tools_2 = t2)})
   all_metric_df <- li_to_df(all_metric_li)
-  write.table(all_metric_df, paste0(o, "/", m, "_metrics.txt"), quote = F, row.names = F)
-  if(plt == "boxplot") {
-    for(i in unique(all_metric_df$metrics)) {
-      pdf(paste0(o, "/", i, "_metrics.pdf"), width = 5, height = 5)
+  write.csv(all_metric_df, paste0(o, "/", m, "_metrics.csv"), quote = F, row.names = F)
+
+  for(i in unique(all_metric_df$metrics)) {
+      pdf(paste0(o, "/", i, "_metrics_box.pdf"), width = 5, height = 5)
       p <- all_metric_df[all_metric_df$metrics==i,] %>% plot_metric_box(i) 
       print(p)
       dev.off()
-    }
-  }
-  else {
-    for(i in unique(all_metric_df$metrics)) {
-      pdf(paste0(o, "/", i, "_metrics.pdf"), width = 5, height = 5)
-      p <- all_metric_df[all_metric_df$metrics==i,] %>% plot_metric_box(i)
+  
+      pdf(paste0(o, "/", i, "_metrics_bar.pdf"), width = 5, height = 5)
+      p <- all_metric_df[all_metric_df$metrics==i,] %>% plot_metric_bar(i)
       print(p)
       dev.off()
-    }
   }
 
 } else { # ROC curve
@@ -83,7 +77,7 @@ if(m != "ROC") {
                                                      tools_1 = t1,
                                                      tools_2 = t2)})
   for(i in 1:length(all_roc_curve_df)) {
-    write.table(all_roc_curve_df[[i]], paste0(o, "/", s[i], "_roc_curve_value.txt"), quote=F, row.names = F)
+    write.csv(all_roc_curve_df[[i]], paste0(o, "/", s[i], "_roc_curve_value.csv"), quote=F, row.names = F)
     pdf(paste0(o, "/", s[i], "_ROC_curve.pdf"), width = 5, height = 5)
     p <- plot_ROC_curve(all_roc_curve_df[[i]], s[i]) 
     print(p)
