c11189f7738561fc8e9c458b040b1bed5092aee1
Qing Chang <46983747+changQing111@users.noreply.github.com>
2023-07-13 17:06:20 +0800

    Update sketch.sh

--- sketch.sh
+++ sketch.sh
@@ -4,5 +4,5 @@ RUN=$1
 OUT=${1}_sketch
 
 prefetch $RUN && \
-$KSSD/kssd dist -p 16 -A -L $SHUF -n 2 -o $OUT --pipecmd "fastq-dump --skip-technical --split-spot -Z" $RUN && \
+$KSSD/kssd dist -p 16 -A -L $SHUF -o $OUT --pipecmd "fastq-dump --skip-technical --split-spot -Z" $RUN && \
 rm -rf $RUN
