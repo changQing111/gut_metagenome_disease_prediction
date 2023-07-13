0cf7ff26c81270f9b7bd71a0b678ae8a674348d9
Qing Chang <46983747+changQing111@users.noreply.github.com>
2023-07-13 19:21:52 +0800

    Update metaphlan_profile.sh

--- metaphlan_profile.sh
+++ metaphlan_profile.sh
@@ -1,11 +1,14 @@
-REF_DB=/home/changqing/Document/metaphlan_db/mpa_v31_CHOCOPhlAn_201901
-INPUT=${1}.fastq
+INPUT=$1
+FASTQ=${1}.fastq
+REF_DB=/home/changqing/Document/metaphlan_db/
 TEMP=${1}.bowtie2.bz2
 OUT=${1}_profile.txt
+LOG=${1}.log
+ERR=${1}.err
 
-metaphlan $INPUT \
+metaphlan $FASTQ \
  --input_type fastq \
  --bowtie2db $REF_DB \
  --bowtie2out $TEMP \
- --nproc 8 \
- -o $OUT
+ --nproc 24 \
+ -o $OUT 1>$LOG 2>$ERR 
